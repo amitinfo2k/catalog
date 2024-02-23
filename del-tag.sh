b9b4d597b6f1d6fe35b3cb261e48900c809dce4c
Amit Wankhede <amit.wankhede@gslab.com>
2024-02-23 16:56:05 +0530

    Trial fix for service node port range

---
+++ del-tag.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+git tag -d v2.0.2
+git push origin :refs/tags/v2.0.2
+
+# Define a string variable with a value
+StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner"
+
+# Iterate the string variable using for loop
+for val in $StringVal; do
+ echo $val
+ git tag -d infra/capi/$val/v2.0.2
+ git push origin :infra/capi/$val/v2.0.2
+done
+
+StringVal1="configsync workload-crds"
+
+# Iterate the string variable using for loop
+for val in $StringVal1; do
+ echo $val
+ git tag -d nephio/core/$val/v2.0.2
+ git push origin :nephio/core/$val/v2.0.2
+done
+
+StringVal2="repository rootsync metallb"
+
+# Iterate the string variable using for loop
+for val in $StringVal2; do
+ echo $val
+ git tag -d distros/sandbox/$val/v2.0.2
+ git push origin :distros/sandbox/$val/v2.0.2
+done
+
+
+StringVal3="rootsync"
+
+# Iterate the string variable using for loop
+for val in $StringVal3; do
+ echo $val
+ git tag -d nephio/optional/$val/v2.0.2
+ git push origin :nephio/optional/$val/v2.0.2
+done
---
+++ test.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+git tag  v2.0.2
+git push origin v2.0.2
+
+# Define a string variable with a value
+StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner"
+
+# Iterate the string variable using for loop
+for val in $StringVal; do
+ echo $val
+ git tag infra/capi/$val/v2.0.2
+ git push origin infra/capi/$val/v2.0.2
+done
+
+StringVal1="configsync workload-crds"
+
+# Iterate the string variable using for loop
+for val in $StringVal1; do
+ echo $val
+ git tag nephio/core/$val/v2.0.2
+ git push origin nephio/core/$val/v2.0.2
+done
+
+StringVal2="repository rootsync metallb"
+
+# Iterate the string variable using for loop
+for val in $StringVal2; do
+ echo $val
+ git tag distros/sandbox/$val/v2.0.2
+ git push origin distros/sandbox/$val/v2.0.2
+done
+
+
+StringVal3="rootsync"
+
+# Iterate the string variable using for loop
+for val in $StringVal3; do
+ echo $val
+ git tag nephio/optional/$val/v2.0.2
+ git push origin nephio/optional/$val/v2.0.2
+done
