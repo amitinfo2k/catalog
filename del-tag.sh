e806ed992522ae3ad77465d2ba043c268c2fe195
Amit Wankhede <amit.wankhede@gslab.com>
2024-02-28 15:14:52 +0530

    updated operator version

--- del-tag.sh
+++ del-tag.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-git tag -d v2.0.2
-git push origin :refs/tags/v2.0.2
+git tag -d v2.0.3
+git push origin :refs/tags/v2.0.3
 
 # Define a string variable with a value
-StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner"
+StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner nephio-workload-cluster"
 
 # Iterate the string variable using for loop
 for val in $StringVal; do
  echo $val
- git tag -d infra/capi/$val/v2.0.2
- git push origin :infra/capi/$val/v2.0.2
+ git tag -d infra/capi/$val/v2.0.3
+ git push origin :infra/capi/$val/v2.0.3
 done
 
 StringVal1="configsync workload-crds"
@@ -18,8 +18,8 @@ StringVal1="configsync workload-crds"
 # Iterate the string variable using for loop
 for val in $StringVal1; do
  echo $val
- git tag -d nephio/core/$val/v2.0.2
- git push origin :nephio/core/$val/v2.0.2
+ git tag -d nephio/core/$val/v2.0.3
+ git push origin :nephio/core/$val/v2.0.3
 done
 
 StringVal2="repository rootsync metallb"
@@ -27,8 +27,8 @@ StringVal2="repository rootsync metallb"
 # Iterate the string variable using for loop
 for val in $StringVal2; do
  echo $val
- git tag -d distros/sandbox/$val/v2.0.2
- git push origin :distros/sandbox/$val/v2.0.2
+ git tag -d distros/sandbox/$val/v2.0.3
+ git push origin :distros/sandbox/$val/v2.0.3
 done
 
 
@@ -37,6 +37,6 @@ StringVal3="rootsync"
 # Iterate the string variable using for loop
 for val in $StringVal3; do
  echo $val
- git tag -d nephio/optional/$val/v2.0.2
- git push origin :nephio/optional/$val/v2.0.2
+ git tag -d nephio/optional/$val/v2.0.3
+ git push origin :nephio/optional/$val/v2.0.3
 done
--- test.sh
+++ test.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-git tag  v2.0.2
-git push origin v2.0.2
+git tag  v2.0.3
+git push origin v2.0.3
 
 # Define a string variable with a value
-StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner"
+StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner nephio-workload-cluster"
 
 # Iterate the string variable using for loop
 for val in $StringVal; do
  echo $val
- git tag infra/capi/$val/v2.0.2
- git push origin infra/capi/$val/v2.0.2
+ git tag infra/capi/$val/v2.0.3
+ git push origin infra/capi/$val/v2.0.3
 done
 
 StringVal1="configsync workload-crds"
@@ -18,8 +18,8 @@ StringVal1="configsync workload-crds"
 # Iterate the string variable using for loop
 for val in $StringVal1; do
  echo $val
- git tag nephio/core/$val/v2.0.2
- git push origin nephio/core/$val/v2.0.2
+ git tag nephio/core/$val/v2.0.3
+ git push origin nephio/core/$val/v2.0.3
 done
 
 StringVal2="repository rootsync metallb"
@@ -27,8 +27,8 @@ StringVal2="repository rootsync metallb"
 # Iterate the string variable using for loop
 for val in $StringVal2; do
  echo $val
- git tag distros/sandbox/$val/v2.0.2
- git push origin distros/sandbox/$val/v2.0.2
+ git tag distros/sandbox/$val/v2.0.3
+ git push origin distros/sandbox/$val/v2.0.3
 done
 
 
@@ -37,6 +37,6 @@ StringVal3="rootsync"
 # Iterate the string variable using for loop
 for val in $StringVal3; do
  echo $val
- git tag nephio/optional/$val/v2.0.2
- git push origin nephio/optional/$val/v2.0.2
+ git tag nephio/optional/$val/v2.0.3
+ git push origin nephio/optional/$val/v2.0.3
 done
