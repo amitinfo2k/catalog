21909a3b3a97ab2740d99f767226e304d7679b00
Amit Wankhede <amit.wankhede@gslab.com>
2024-03-14 12:26:58 +0530

    addd cassandra pkg

--- assign-tag.sh
+++ assign-tag.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-git tag  v2.0.4
-git push origin v2.0.4
+
+TAG=v2.0.4
+
+git tag  $TAG
+git push origin $TAG
 
 # Define a string variable with a value
 StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner nephio-workload-cluster"
@@ -9,8 +12,8 @@ StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner ne
 # Iterate the string variable using for loop
 for val in $StringVal; do
  echo $val
- git tag infra/capi/$val/v2.0.4
- git push origin infra/capi/$val/v2.0.4
+ git tag infra/capi/$val/$TAG
+ git push origin infra/capi/$val/$TAG
 done
 
 StringVal1="configsync workload-crds nephio-operator"
@@ -18,8 +21,8 @@ StringVal1="configsync workload-crds nephio-operator"
 # Iterate the string variable using for loop
 for val in $StringVal1; do
  echo $val
- git tag nephio/core/$val/v2.0.4
- git push origin nephio/core/$val/v2.0.4
+ git tag nephio/core/$val/$TAG
+ git push origin nephio/core/$val/$TAG
 done
 
 StringVal2="repository rootsync metallb"
@@ -27,8 +30,8 @@ StringVal2="repository rootsync metallb"
 # Iterate the string variable using for loop
 for val in $StringVal2; do
  echo $val
- git tag distros/sandbox/$val/v2.0.4
- git push origin distros/sandbox/$val/v2.0.4
+ git tag distros/sandbox/$val/$TAG
+ git push origin distros/sandbox/$val/$TAG
 done
 
 
@@ -37,8 +40,8 @@ StringVal3="rootsync"
 # Iterate the string variable using for loop
 for val in $StringVal3; do
  echo $val
- git tag nephio/optional/$val/v2.0.4
- git push origin nephio/optional/$val/v2.0.4
+ git tag nephio/optional/$val/$TAG
+ git push origin nephio/optional/$val/$TAG
 done
 
 StringVal4="pkg-example-hss-bp pkg-example-mme-bp sdcore4g-operator pkg-example-pcrf-bp pkg-example-spgwc-bp pkg-example-config4g-bp sdcore4g-cp sdcore4g upf"
@@ -46,9 +49,16 @@ StringVal4="pkg-example-hss-bp pkg-example-mme-bp sdcore4g-operator pkg-example-
 # Iterate the string variable using for loop
 for val in $StringVal4; do
  echo $val
- git tag workloads/sdcore4g/$val/v2.0.4
- git push origin workloads/sdcore4g/$val/v2.0.4
+ git tag workloads/sdcore4g/$val/$TAG
+ git push origin workloads/sdcore4g/$val/$TAG
 done
 
+StringVal5="cassandra"
 
+# Iterate the string variable using for loop
+for val in $StringVal5; do
+ echo $val
+ git tag workloads/tools/$val/$TAG
+ git push origin workloads/tools/$val/$TAG
+done
 
--- del-tag.sh
+++ del-tag.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-git tag -d v2.0.4
-git push origin :refs/tags/v2.0.4
+TAG=v2.0.4
+
+git tag -d $TAG
+git push origin :refs/tags/$TAG
 
 # Define a string variable with a value
 StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner nephio-workload-cluster"
@@ -9,8 +11,8 @@ StringVal="kindnet multus vlanindex cluster-capi-kind  local-path-provisioner ne
 # Iterate the string variable using for loop
 for val in $StringVal; do
  echo $val
- git tag -d infra/capi/$val/v2.0.4
- git push origin :infra/capi/$val/v2.0.4
+ git tag -d infra/capi/$val/$TAG
+ git push origin :infra/capi/$val/$TAG
 done
 
 StringVal1="configsync workload-crds"
@@ -18,8 +20,8 @@ StringVal1="configsync workload-crds"
 # Iterate the string variable using for loop
 for val in $StringVal1; do
  echo $val
- git tag -d nephio/core/$val/v2.0.4
- git push origin :nephio/core/$val/v2.0.4
+ git tag -d nephio/core/$val/$TAG
+ git push origin :nephio/core/$val/$TAG
 done
 
 StringVal2="repository rootsync metallb"
@@ -27,8 +29,8 @@ StringVal2="repository rootsync metallb"
 # Iterate the string variable using for loop
 for val in $StringVal2; do
  echo $val
- git tag -d distros/sandbox/$val/v2.0.4
- git push origin :distros/sandbox/$val/v2.0.4
+ git tag -d distros/sandbox/$val/$TAG
+ git push origin :distros/sandbox/$val/$TAG
 done
 
 
@@ -37,8 +39,8 @@ StringVal3="rootsync"
 # Iterate the string variable using for loop
 for val in $StringVal3; do
  echo $val
- git tag -d nephio/optional/$val/v2.0.4
- git push origin :nephio/optional/$val/v2.0.4
+ git tag -d nephio/optional/$val/$TAG
+ git push origin :nephio/optional/$val/$TAG
 done
 
 StringVal4="pkg-example-hss-bp pkg-example-mme-bp sdcore4g-operator pkg-example-pcrf-bp pkg-example-spgwc-bp pkg-example-config4g-bp sdcore4g-cp sdcore4g upf"
@@ -46,8 +48,8 @@ StringVal4="pkg-example-hss-bp pkg-example-mme-bp sdcore4g-operator pkg-example-
 # Iterate the string variable using for loop
 for val in $StringVal4; do
  echo $val
- git tag -d workloads/sdcore4g/$val/v2.0.4
- git push origin :workloads/sdcore4g/$val/v2.0.4
+ git tag -d workloads/sdcore4g/$val/$TAG
+ git push origin :workloads/sdcore4g/$val/$TAG
 done
 
 
